c2d401f91216d0000a05a74682ff1430cbb8d110
Aaron Miller <amiller26@zagmail.gonzaga.edu>
2020-11-29 17:16:44 -0800

    commit

---
+++ BirthdayList/Interest.swift
@@ -0,0 +1,18 @@
+//
+//  Interest.swift
+//  BirthdayList
+//
+//  Created by Miller, Aaron Paul on 11/29/20.
+//
+
+import Foundation
+
+class Interest {
+    var name: String
+    var webLink: URL
+    
+    init(name: String, webLink: URL) {
+        self.name = name
+        self.webLink = webLink
+    }
+}
