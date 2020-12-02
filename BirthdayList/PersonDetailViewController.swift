10f2ede351543910b59c44db86579ee6f02e1c82
Charles Walker <walkercharlesmw99@gmail.com>
2020-12-01 20:48:24 -0800

    kuk

--- BirthdayList/PersonDetailViewController.swift
+++ BirthdayList/PersonDetailViewController.swift
@@ -34,20 +34,24 @@ class PersonDetailViewController: UIViewController {
         if let friend = friendOptional, let personName = friend.name, let birthDate = friend.birthday, let tempString = friend.interests {
             destLabel.text = "Name: " + personName
             let interests = tempString.components(separatedBy: "* ")
+            print(interests.count)
+            print("\(interests)")
             interest1Button.setTitle(interests[0], for: .normal)
             self.interest2Button.isHidden = true
             self.interest3Button.isHidden = true
             if interests.count == 2 {
                 interest2Button.setTitle(interests[1], for: .normal)
                 self.interest2Button.isHidden = false
-
+                print("succses")
             }
             if interests.count == 3 {
                 interest2Button.setTitle(interests[1], for: .normal)
                 interest3Button.setTitle(interests[2], for: .normal)
                 self.interest2Button.isHidden = false
                 self.interest3Button.isHidden = false
+                print("sdfsa")
             }
+           
             if let bDate = birthDate as Date? {
                 startDateLabel.text = "Start Date: \(dateFormatter.string(from: bDate))"
             }
